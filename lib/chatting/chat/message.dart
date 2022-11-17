f0ca1fca7fe86b12d8aca137668a95c520c21d1b
seheon Kim <seheon.emma@gmail.com>
2022-11-17 15:59:36 +1000

    fix : chat screen && chat message feature

--- lib/chatting/chat/message.dart
+++ lib/chatting/chat/message.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_demo/chatting/chat/chat_bubble.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Message extends StatelessWidget {
   const Message({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
     return StreamBuilder(
         stream: FirebaseFirestore.instance
             .collection('chat')
@@ -22,7 +25,10 @@ class Message extends StatelessWidget {
             reverse: true,
             itemCount: chatDocs.length,
             itemBuilder: (context, index){
-              return Text(chatDocs[index]['text']);
+              return ChatBubble(
+                  chatDocs[index]['text'],
+                  chatDocs[index]['userID'].toString()==user!.uid,
+              );
             },
           );
         },
